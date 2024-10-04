3928f56c50c26b7ce00b3523f84c865f90411c40
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-04 14:29:43 +0900

    Title: 규칙에 따라 밀기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241004/규칙에 따라 밀기/push-by-the-rules.py
+++ 241004/규칙에 따라 밀기/push-by-the-rules.py
@@ -5,6 +5,6 @@ l = order.count('L')
 shift = r - l
 if shift < 0: # shift to left
     n = n[abs(shift):] + n[:abs(shift)]
-else: # shift to right
+elif shift > 0: # shift to right
     n = n[:len(n) - shift] + n[len(n) - shift :]
 print(n)
