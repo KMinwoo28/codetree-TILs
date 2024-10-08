73dac010e07a3b422e0f31450c93df693fe10d62
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-08 22:56:30 +0900

    Title: DateTime to DateTime, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 241008/DateTime to DateTime/datetime-to-datetime.py
+++ 241008/DateTime to DateTime/datetime-to-datetime.py
@@ -1,5 +1,9 @@
 a, b, c = map(int,input().split())
-if b<11 or (b == 11 and c<11):
+#if a<11 or (a == 11 and b<11):
+#    print(-1)
+#else:
+s = (a*24*60 + b*60 + c) - (11*24*60 + 11*60 + 11) 
+if s < 0:
     print(-1)
 else:
-    print((a*24*60 + b*60 + c) - (11*24*60 + 11*60 + 11) )
+    print(s)
