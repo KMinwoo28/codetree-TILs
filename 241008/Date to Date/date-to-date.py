b7b370b5b88575c4b21abf4a6ab8939aba7fe5a8
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-08 22:15:39 +0900

    Title: Date to Date, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241008/Date to Date/date-to-date.py
@@ -0,0 +1,8 @@
+m1, d1, m2, d2 = map(int,input().split())
+
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+s = num_of_days[m1] - d1 + d2 + 1
+for i in range(m1+1, m2):
+    s += num_of_days[i+1]
+print(s)
