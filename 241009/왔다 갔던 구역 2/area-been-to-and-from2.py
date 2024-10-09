7567f15b479d4f781ea7be8c1f77f6685dc034e9
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-09 15:54:42 +0900

    Title: 왔다 갔던 구역 2, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 241009/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -5,15 +5,18 @@ index = 500
 for i in range(n):
     x, order = input().split()
     x = int(x)
-    
+    #print(f'move {index} to', end = " ")
     if order == 'R':
         for s in range(index, index + x):
-            lst[s] += 1        
+            lst[s] += 1
         index += x
     elif order == 'L':
-        for s in range(index, index - x , -1):
+        for s in range(index-1, index - x-1, -1):
             lst[s] += 1
         index -= x
+    #print(index)
+    #print(lst[480:520])
+
 cnt = 0
 for i in range(len(lst)):
     if lst[i] >= 2:
