92f6ab95f1ab6410235f1082159328607e5cdf0b
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-15 15:49:30 +0900

    Title: 거울에 레이저 쏘기 2, Time: 205ms, Memory: 48MB, Status: Passed - CodeTree

--- 241015/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 241015/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -2,31 +2,25 @@
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-direct = {
-    "R" : 0,
-    'D' : 1,
-    'L' : 2,
-    'U' : 3
-}
 def left_mir(d): # \일때
-    if d == 'U':
-        return 'L'
-    elif d == 'L':
-        return 'U'
-    elif d == "R":
-        return 'D'
-    elif d == 'D':
-        return 'R'
+    if d == 3:
+        return 2
+    elif d == 2:
+        return 3
+    elif d == 0:
+        return 1
+    elif d == 1:
+        return 0
 
 def right_mir(d): # /일때
-    if d == 'U':
-        return 'R'
-    elif d == 'L':
-        return 'U'
-    elif d == "R":
-        return 'U'
-    elif d == 'D':
-        return 'L'
+    if d == 3:
+        return 0
+    elif d == 2:
+        return 1
+    elif d == 0:
+        return 3
+    elif d == 1:
+        return 2
 
 N = int(input())
 def in_range(x, y):
@@ -44,25 +38,25 @@ r, c = 0, 0
 match m:
     case 0:
         r, c = 0, re
-        d = 'D'
+        d = 1
     case 1:
         r, c = re, N-1
-        d = 'L'
+        d = 2
     case 2:
         r, c = N-1, (N-1) - re
-        d = 'U'
+        d = 3
     case 3:
         r, c = (N-1) - re, 0
-        d = 'R'
+        d = 0
 cnt = 0
 while True:
 
     cnt += 1
     s = mirror[r][c]
     n_d = left_mir(d) if s == '\\' else right_mir(d)
-    nr, nc = r + dx[direct[n_d]], c + dy[direct[n_d]]
+    nr, nc = r + dx[n_d], c + dy[n_d]
     if not in_range(nr, nc):
         break
-    r, c = r + dx[direct[n_d]], c + dy[direct[n_d]]
+    r, c = r + dx[n_d], c + dy[n_d]
     d = n_d
 print(cnt)
