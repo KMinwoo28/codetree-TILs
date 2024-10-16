42726c8b3bf6b352c39db9e440ea906cb5b0c581
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-16 11:19:07 +0900

    Title: 일렬로 서있는 소 2, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241016/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,10 @@
+n = int(input())
+cow = list(map(int,input().split()))
+
+cnt = 0
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if cow[i] <= cow[j] and cow[j] <= cow[k]:
+                cnt +=1
+print(cnt)
