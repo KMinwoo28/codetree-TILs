0b39652973a1532a89fe3ec6fac04f192d24d331
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-18 13:16:15 +0900

    Title: 개발팀의 능력, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241018/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 241018/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -2,7 +2,7 @@ lst = list(map(int,input().split()))
 
 def diff(i,j,k,l):
     sum1 = lst[i] + lst[j]
-    sum2 = lst[j] + lst[k]
+    sum2 = lst[k] + lst[l]
     sum3 = sum(lst) - sum1 - sum2
     if sum1 != sum2 and sum2 != sum3 and sum3 != sum1:
         return max(sum1, sum2, sum3) - min(sum1, sum2, sum3)
