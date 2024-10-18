1c92f925c554cc0611e34a3f76cbc45d260fb810
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-18 13:03:02 +0900

    Title: 개발팀의 능력, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241018/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,27 @@
+lst = list(map(int,input().split()))
+
+def diff(i,j,k,l):
+    sum1 = lst[i] + lst[j]
+    sum2 = lst[j] + lst[k]
+    sum3 = sum(lst) - sum1 - sum2
+    rst = abs(sum3 - sum2)
+    rst = max(rst, abs(sum3 - sum1))
+    rst = max(rst, abs(sum2 - sum1))
+    if sum1 == sum2 or sum2 == sum3 or sum1 == sum3:
+        return False, rst
+    else:
+        return True, rst
+m = 20000
+for i in range(5):
+    for j in range(1,5):
+        for k in range(5):
+            for l in range(1,5):
+                if k == i or k == j or l == i or l == j:
+                    continue
+                av, s = diff(i,j,k,l)
+                if av:
+                    m = min(m,s)
+if m != 20000:
+    print(m)
+else:
+    print(-1)
