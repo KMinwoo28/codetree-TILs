e58dc0ebb76f8333773959bb6a49656a678d715e
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-18 12:08:10 +0900

    Title: 특정 수와 근접한 합, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 241018/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 241018/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -4,7 +4,7 @@ N, S = map(int,input().split())
 lst = list(map(int,input().split()))
 m = INT_MAX
 for i in range(N-1):
-    for j in range(i,N):
+    for j in range(i+1,N):
         ext = lst[:i] + lst[i+1:j] + lst[j+1:]
         m = min(m, abs(S - sum(ext)))
 print(m)
