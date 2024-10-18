aa96c24fc3dab606b91f88f22b76fa8ddddf3793
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-18 09:49:36 +0900

    Title: 이상한 진수 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241018/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,15 @@
+import sys
+INT_MIN = -sys.maxsize
+binary = list(map(int,list(input())))
+ans = INT_MIN
+
+for i in range(len(binary)):
+    binary[i] = 1 - binary[i]
+
+    num = 0
+    for j in range(len(binary)):
+        num = num * 2 + binary[j]
+    ans = max(ans, num)
+
+    binary[i] = 1 - binary[i]
+print(ans)
